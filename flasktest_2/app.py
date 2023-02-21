2d2c5a50a423d10f3d87282a6416c4b723c113e8
Hants Williams <hantsawilliams@gmail.com>
2023-02-20 19:46:52 -0500

    traefik updates

--- flasktest_2/app.py
+++ flasktest_2/app.py
@@ -7,4 +7,4 @@ def hello():
     return 'Hello from a remote server on Flask (server 2)! - SUBDOMAIN 1'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=3000)
