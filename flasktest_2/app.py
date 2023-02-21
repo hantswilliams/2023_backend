834e9bfd503ab2d04cdef1cf63b5ec5d2e2801db
Hants Williams <hantsawilliams@gmail.com>
2023-02-20 19:42:59 -0500

    testing with flask

--- flasktest_2/app.py
+++ flasktest_2/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello from a remote server on Flask (server 2)!'
+    return 'Hello from a remote server on Flask (server 2)! - SUBDOMAIN 1'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=80)
--- traefik_test/readme.md
+++ traefik_test/readme.md
@@ -8,3 +8,7 @@ Run command:
 ```
 docker run -d -p 80:80 -p 443:443 -v /var/run/docker.sock:/var/run/docker.sock -v /path/to/traefik.toml:/etc/traefik/traefik.toml traefik
 ``` 
+
+
+Before doing this stuff, 
+still need to setup DNS stuff though - should optimize this later 
