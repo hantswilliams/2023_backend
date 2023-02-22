32a683d2cdbe20251df522477937e2233c22756a
Hants Williams <hantsawilliams@gmail.com>
2023-02-22 08:08:21 -0500

    changes

--- flasktest_1/app.py
+++ flasktest_1/app.py
@@ -7,4 +7,4 @@ def hello():
     return 'Hello from a remote server on Flask (server 1) - MAIN PAGE!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=5000)
